2f8891b1146f627a3598ee24f24ca1b5a5ac008b
Petromir Kolev <petromirkolev@gmail.com>
2026-02-05 10:40:11 +0200

    wip(test): derived units tests

--- tests/derived.spec.ts
+++ tests/derived.spec.ts
@@ -2,7 +2,24 @@ import { test, expect } from './src/test-options';
 
 test.describe('Derived metrics test suite', () => {
   test.describe('Heat index', () => {
-    test('Heat index is calculated properly', async ({ dashboard }) => {});
+    test('Heat index is calculated properly', async ({ dashboard }) => {
+      await dashboard.gotoWithWsSpy();
+      await dashboard.waitForHello();
+
+      console.log(await dashboard.tempValue.textContent());
+      console.log(await dashboard.heatIndex.textContent());
+
+      const temp = Number((await dashboard.tempValue.textContent())?.trim());
+      const heat = Number((await dashboard.heatIndex.textContent())?.trim());
+
+      console.log(temp);
+      console.log(heat);
+
+      expect(Number.isFinite(temp)).toBe(true);
+      expect(Number.isFinite(heat)).toBe(true);
+
+      expect(heat).toBeGreaterThan(temp);
+    });
   });
   test.describe('Dew point', () => {});
   test.describe('Comfort index', () => {});
